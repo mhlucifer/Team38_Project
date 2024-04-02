a28aa040a8bf93d68fe761c37a3656c6e1ecddf8
tomato <2418378617@qq.com>
2024-04-03 08:58:50 +1100

    modify the app.py and add the new api for main page

--- app.py
+++ app.py
@@ -2,10 +2,14 @@ from flask import Flask,render_template
 
 app = Flask(__name__) 
 
+
 @app.route('/')
 def hello_world(): 
-    return render_template('index.html')
+    return "hello world"
 
+
+@app.route('/index')
+def index(): return render_template('index.html')
 
 
 if __name__ == '__main__':

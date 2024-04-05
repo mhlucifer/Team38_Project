0d31925d456a6102f3aa993f59b6b0b18cb4d341
tomato <2418378617@qq.com>
2024-04-05 22:51:19 +1100

    fix bug for api

--- app.py
+++ app.py
@@ -1,13 +1,26 @@
-from flask import Flask,render_template
+from flask import Flask, render_template
 
 app = Flask(__name__) 
 
 
-
 @app.route('/')
-def hello_world(): 
+def index_page():
     return render_template('index.html')
 
+
+@app.route('/handle')
+def handling_page():
+    return render_template('handling.html')
+
+
+@app.route('/location')
+def location_page():
+    return render_template('location.html')
+
+
+@app.route('/behaviour')
+def behaviour_page():
+    return render_template('behaviour.html')
 
 
 if __name__ == '__main__':

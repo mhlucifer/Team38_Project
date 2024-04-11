261e9fc56a67b29a3029dc1901bed57c2a33fa26
tomato <2418378617@qq.com>
2024-04-11 23:25:49 +1000

    add the login page

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__) 
 
 
-@app.route('/')
+@app.route('/index')
 def index_page():
     return render_template('index.html')
 
@@ -21,6 +21,25 @@ def location_page():
 @app.route('/behaviour')
 def behaviour_page():
     return render_template('behaviour.html')
+
+
+@app.route('/test')
+def test_page():
+    return render_template('main.html')
+
+
+@app.route('/')
+def login_page1():
+    return render_template('login.html')
+
+
+@app.route('/login-password', methods=['POST'])
+def login_page():
+    password = request.form['password']
+    if password == '1234':
+        return jsonify({"success": True, "message": "Login successful"})
+    else:
+        return jsonify({"success": False, "message": "Invalid password,Please try again"}), 401
 
 
 if __name__ == '__main__':

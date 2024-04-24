087088baefc34849e9074ea982ac956973ef1d04
pavheer <162963180+pavheer@users.noreply.github.com>
2024-04-24 21:18:08 +1000

    "added_comments"

---
+++ Backend_code/data_cleansing_addition_to_csv.py
@@ -0,0 +1,91 @@
+#This script is used to cleanse and wrangle the raw dataset
+
+# DONT RUN THIS FILE! IT TAKES A LONG TIME, THE OUTPUT IS SAVED IN STATIC 
+#Import the libraries
+import pandas as pd
+import pandas as pd
+import mysql.connector
+from mysql.connector import errorcode
+from sqlalchemy import create_engine
+import geopandas as gpd
+from shapely.geometry import Point
+
+#Read the data 
+df=pd.read_csv('/Users/pavneetheer/Downloads/records24-04-2024/records24-04-2024.csv')
+#Provides the information about the data 
+df.info()
+#Print the column names 
+column_names = df.columns.tolist()
+print(column_names)
+#Only the selected columns are kept 
+columns_to_select= ['year', 'month','day', 'stateProvince', 'decimalLatitude', 'decimalLongitude']
+df=df[columns_to_select]
+#Check for data types of different columns 
+print(df.dtypes)
+#Check for null values 
+null_values = df.isnull().sum()
+#Drop columns with null values 
+df=df.dropna()
+df.info()
+
+df['year'] = df['year'].astype(int)
+df['month'] = df['month'].astype(int)
+df['day'] = df['day'].astype(int)
+
+new_names = {'year': 'Year', 'month': 'Month', 'day': 'Day', 'decimalLatitude': 'Latitude', 'decimalLongitude': 'Longitude' }
+df = df.rename(columns=new_names)
+
+
+
+suburbs = gpd.read_file('/Users/pavneetheer/Downloads/SAL_2021_AUST_GDA94_SHP/SAL_2021_AUST_GDA94.shp')
+df['Suburb'] = None
+
+n=0
+for index, row in df.iterrows():
+    lat = row["Latitude"]
+    lng = row["Longitude"]
+    point = Point((lng, lat))
+    for _, row in suburbs.iterrows():
+        polygon = row['geometry']
+        if point.within(polygon):
+            df.at[index, "Suburb"] = row['SAL_NAME21']
+            n=n+1
+            print(n)
+
+print("done")
+
+df.to_csv("Suburbs_data.csv")
+
+df.reset_index(drop=True, inplace=True)
+
+df = df.rename(columns={'stateProvince': 'State'})
+
+config = {
+  'host':'db-infotoad.mysql.database.azure.com',
+  'user':'infotoad38admin',
+  'password':'cO52LFKBwNYrmXVJvbdM',
+  'database':'dbtoad',
+  'client_flags': [mysql.connector.ClientFlag.SSL],
+  'ssl_ca': '/Users/pavneetheer/Downloads/DigiCertGlobalRootCA.crt.pem'
+}
+
+conn = mysql.connector.connect(**config)
+mycursor=conn.cursor()
+#Query for creating a table 
+query = (
+    "CREATE TABLE data ("
+    "ID INT AUTO_INCREMENT PRIMARY KEY,"
+    "Latitude DOUBLE, "
+    "Longitude Double,"
+    "State VARCHAR(255), "
+    "Suburb VARCHAR(255), "
+    "Year INT, "
+    "Month INT, "
+    "Day INT)"
+)
+
+mycursor.execute(query)
+
+#Creating a connection to our db
+engine = create_engine("mysql+mysqlconnector://infotoad38admin:cO52LFKBwNYrmXVJvbdM@db-infotoad.mysql.database.azure.com/dbtoad")
+df.to_sql('data', con=engine, if_exists='replace', index=False)

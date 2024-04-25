4d0d466a50c94e13357f403a1391acab6e8b4e92
tomato <2418378617@qq.com>
2024-04-25 21:28:55 +1000

    test the version

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, jsonify
-
 app = Flask(__name__) 
+
 
 
 @app.route('/index')
@@ -23,19 +23,9 @@ def behaviour_page():
     return render_template('behaviour.html')
 
 
-@app.route('/test')
-def test_page():
-    return render_template('main.html')
-
-
 @app.route('/')
 def login_page1():
     return render_template('login.html')
-
-
-@app.route('/main')
-def main_page1():
-    return render_template('main.html')
 
 
 @app.route('/login-password', methods=['POST'])
@@ -45,6 +35,36 @@ def login_page():
         return jsonify({"success": True, "message": "Login successful"})
     else:
         return jsonify({"success": False, "message": "Invalid password,Please try again"}), 401
+
+
+@app.route('/test')
+def main_page1():
+    return render_template('test.html')
+
+
+@app.route('/take_action')
+def tack_action():
+    return render_template('take_action.html')
+
+
+@app.route('/about_us')
+def about_us():
+    return render_template('about_us.html')
+
+
+@app.route('/history')
+def history():
+    return render_template('history.html')
+
+
+@app.route('/identifier')
+def identifier():
+    return render_template('identifier.html')
+
+
+@app.route('/test_your_knowlege')
+def test_your_knowledge():
+    return render_template('test_your_knowlege.html')
 
 
 if __name__ == '__main__':
